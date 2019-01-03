a6e2a37b03664295a3921c16480a59005e8e1865
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2019-01-03 14:24:57 +0100

    try yapf, not bad ;)

--- sudoku_solver/board.py
+++ sudoku_solver/board.py
@@ -1,5 +1,6 @@
 from .error import AmbiguousError
 
+
 class Board:
     BasicAlphabet = '123456789'
 
@@ -13,7 +14,9 @@ class Board:
 
     def from_file(self, filename, alphabet=BasicAlphabet, separator=','):
         with open(filename) as f:
-            self.__field = [l.split(separator) for l in f.read().split('\n') if l != '']
+            self.__field = [
+                l.split(separator) for l in f.read().split('\n') if l != ''
+            ]
 
     def field(self):
         return self.__field
@@ -28,7 +31,8 @@ class Board:
         if len(self.__field) != len(self.__alphabet):
             return False
 
-        if not all([len(line) == len(self.__alphabet) for line in self.__field]):
+        if not all(
+            [len(line) == len(self.__alphabet) for line in self.__field]):
             return False
 
         for i in range(0, len(self.__alphabet)):
@@ -58,7 +62,9 @@ class Board:
         tried = 0
         while len(positions) > 0:
             if tried > len(positions):
-                raise AmbiguousError("It's not possible to solve this one or there are more than one solution")
+                raise AmbiguousError(
+                    "It's not possible to solve this one or there are more than one solution"
+                )
             position = positions.pop(0)
 
             row = self.__row(self.__row_of(*position))
@@ -77,11 +83,9 @@ class Board:
 
     def __fill_positions(self):
         max_l = len(self.__alphabet)
-        return [
-            (i % max_l, i // max_l)
-            for i in range(0, pow(len(self.__alphabet), 2))
-            if self.__field[i // max_l][i % max_l] == '_'
-        ]
+        return [(i % max_l, i // max_l)
+                for i in range(0, pow(len(self.__alphabet), 2))
+                if self.__field[i // max_l][i % max_l] == '_']
 
     def __possible_values(self, row, column, block):
         existing_values = set(row + column + block)
@@ -96,7 +100,7 @@ class Board:
     def __block(self, i):
         y = (i // 3) * 3
         x = (i % 3) * 3
-        return [v for l in self.__field[y:y+3] for v in l[x:x+3]]
+        return [v for l in self.__field[y:y + 3] for v in l[x:x + 3]]
 
     def __row_of(self, x, y):
         return y
